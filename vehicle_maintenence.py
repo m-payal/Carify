5b2a1c12cba97bb415655e77c28fbc02f3aad29b
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2021-12-14 12:27:29 +0530

    Update vehicle_maintenence.py

--- vehicle_maintenence.py
+++ vehicle_maintenence.py
@@ -183,28 +183,28 @@ if st.sidebar.checkbox("KNOW information about your CAR.", False):
 if st.sidebar.checkbox("COMPARE TWO CARS?", False):
     st.subheader("COMPARE TWO CAR")
     st.write("CAR 1")
-    selectt = st.selectbox('Company Name', ['Hyundai','Ford','Honda','KIA'])
+    selectt = st.selectbox('Company Name', ['Hyundai','Ford','Honda','KIA'],key=1)
     if selectt =='Hyundai':
-        selectt1 = st.selectbox('Model Name', ['All New Santro', 'Creta', 'Grand i10','i20'])
+        selectt1 = st.selectbox('Model Name', ['All New Santro', 'Creta', 'Grand i10','i20'],key=1)
     if selectt == 'Ford':
-        selectt1 = st.selectbox('Model Name', ['Ecosports', 'Figo',])
+        selectt1 = st.selectbox('Model Name', ['Ecosports', 'Figo',],key=1)
     if selectt == 'Honda':
-        selectt1 = st.selectbox('Model Name', ['Amaze', 'City(2014)', 'WR-V'])
+        selectt1 = st.selectbox('Model Name', ['Amaze', 'City(2014)', 'WR-V'],key=1)
     if selectt == 'KIA':
-        selectt1 = st.selectbox('Model Name', ['Carnival'])
+        selectt1 = st.selectbox('Model Name', ['Carnival'],key=1)
 
-    selectt2 = st.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'])
-    selectt4 = st.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'])
+    selectt2 = st.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'],key=1)
+    selectt4 = st.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'],key=1)
     selectt3 = st.slider("Age of the vehicle.", 0, 200)
 
     st.write("CAR 2")
-    selecttt = st.selectbox('Company', ['Hyundai', 'Ford', 'Honda', 'KIA'])
+    selecttt = st.selectbox('Company', ['Hyundai', 'Ford', 'Honda', 'KIA'],key=2)
     if selecttt == 'Hyundai':
-        selecttt1 = st.selectbox('Model Name.', ['All New Santro', 'Creta', 'Grand i10', 'i20'])
+        selecttt1 = st.selectbox('Model Name.', ['All New Santro', 'Creta', 'Grand i10', 'i20'],key=2)
     if selectt == 'Ford':
-        selecttt1 = st.selectbox('Model Name.', ['Ecosports', 'Figo', ])
+        selecttt1 = st.selectbox('Model Name.', ['Ecosports', 'Figo', ],key=2)
     if selecttt == 'Honda':
-        selecttt1 = st.selectbox('Model Name.', ['Amaze', 'City(2014)', 'WR-V'])
+        selecttt1 = st.selectbox('Model Name.', ['Amaze', 'City(2014)', 'WR-V'],key=2)
     if selectt == 'KIA':
         selecttt1 = st.selectbox('Model Name.', ['Carnival'])
 
