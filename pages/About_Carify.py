91097e4f302027eb799c9e895807729df1e29329
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-28 00:21:52 +0530

    Update About_Carify.py

--- pages/About_Carify.py
+++ pages/About_Carify.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 import numpy as np
 
-st.set_page_config(page_title="ABOUT US", page_icon="📈")
+st.set_page_config(page_title="ABOUT US", page_icon="🚖")
 
 st.sidebar.header("ABOUT US")
 st.subheader("ABOUT CARIFY")
