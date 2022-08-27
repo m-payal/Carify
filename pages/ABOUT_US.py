178a86c2da251f2c6364fad6f90571b8a1b00428
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-27 23:41:53 +0530

    Create ABOUT_US.py

---
+++ pages/ABOUT_US.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="ABOUT US", page_icon="📈")
+
+st.markdown("# ABOUT US")
+st.sidebar.header("ABOUT US")
+st.subheader("ABOUT CARIFY")
+st.write("In Today’s world with so many cars, models in the market, it is hard to find out which car has a high maintenance cost/index that is authentic source.While buying a car we spend a lot of time different features and also maintenance. But there's no platform which tells us the maintenance cost that we'll have to pay after buying the car. A system that shows the health and Maintenance Index of various components of car models or car parts based on multiple factors is needed. We have created this platform for all these problems")
+st.write("In our system we show the health and maintenance Index of various components of car models or car parts based on multiple factors.Also the probablity of replacement of a particular car component after the car is bought. We can also compare two different car models based on maintenence.This will help new buyers to understand the maintenance costs of a certain model and probability of which car part requires more often servicing /change, OEM’s to understand which part is requiring frequent change so they can improve that component and needs to be recalled and made better in the new models ")
+st.subheader('"Know your car\'s life before you buy it!"')
+st.text("\n")
+st.text("\n")
+st.text("\n")
+st.text("\n")
+st.text("\n")
+st.text(" \n BY TEAM \n Shreya Pawaskar \n Akshata Jedhe \n Juee Ashtaputre \n Payal Mehta \n Riya Kulkarni")
+st.button("Re-run")
