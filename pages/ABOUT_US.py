c27d76694a60a41c52685c3855ce78cf9288b4e3
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-27 23:43:44 +0530

    Update ABOUT_US.py

--- pages/ABOUT_US.py
+++ pages/ABOUT_US.py
@@ -4,7 +4,6 @@ import numpy as np
 
 st.set_page_config(page_title="ABOUT US", page_icon="📈")
 
-st.markdown("# ABOUT US")
 st.sidebar.header("ABOUT US")
 st.subheader("ABOUT CARIFY")
 st.write("In Today’s world with so many cars, models in the market, it is hard to find out which car has a high maintenance cost/index that is authentic source.While buying a car we spend a lot of time different features and also maintenance. But there's no platform which tells us the maintenance cost that we'll have to pay after buying the car. A system that shows the health and Maintenance Index of various components of car models or car parts based on multiple factors is needed. We have created this platform for all these problems")
