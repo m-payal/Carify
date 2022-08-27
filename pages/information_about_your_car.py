dac70ff0fc625f1bd6e7743611693276d6e7273b
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-27 23:50:10 +0530

    Update information_about_your_car.py

--- pages/information_about_your_car.py
+++ pages/information_about_your_car.py
@@ -4,21 +4,21 @@ import numpy as np
 
 st.set_page_config(page_title="Information about your Car", page_icon="📈")
 
-st.sidebar.header("Know more about your car")
-st.sidebar.subheader("Please fill in the details")
-select = st.sidebar.selectbox('Company', ['Hyundai','Ford','Honda','KIA'])
+st.header("Know more about your car")
+st.ubheader("Please fill in the details")
+select = st.selectbox('Company', ['Hyundai','Ford','Honda','KIA'])
 if select =='Hyundai':
-    select1 = st.sidebar.selectbox('Model', ['All New Santro', 'Creta', 'Grand i10','i20'])
+    select1 = st.selectbox('Model', ['All New Santro', 'Creta', 'Grand i10','i20'])
 if select == 'Ford':
-    select1 = st.sidebar.selectbox('Model', ['Ecosports', 'Figo',])
+    select1 = st.selectbox('Model', ['Ecosports', 'Figo',])
 if select == 'Honda':
-    select1 = st.sidebar.selectbox('Model', ['Amaze', 'City(2014)', 'WR-V'])
+    select1 = st.selectbox('Model', ['Amaze', 'City(2014)', 'WR-V'])
 if select == 'KIA':
-    select1 = st.sidebar.selectbox('Model', ['Carnival'])
+    select1 = st.selectbox('Model', ['Carnival'])
 
-select2 = st.sidebar.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'])
-select4 = st.sidebar.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'])
-select3 = st.sidebar.text_input('Enter Age of Vehicle here:')
+select2 = st.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'])
+select4 = st.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'])
+select3 = st.text_input('Enter Age of Vehicle here:')
 st.write("Check show data to get the information.")
     
 st.text("\n")
