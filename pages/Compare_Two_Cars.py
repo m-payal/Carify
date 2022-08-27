5367dc3e42c10856c1d9afdf625627f533776014
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-28 00:11:33 +0530

    Update Compare_Two_Cars.py

--- pages/Compare_Two_Cars.py
+++ pages/Compare_Two_Cars.py
@@ -14,31 +14,31 @@ str1.subheader("COMPARE TWO CAR")
 str1.write("CAR 1")
 selectt = str1.selectbox('Company Name', ['Hyundai','Ford','Honda','KIA'],key=4)
 if selectt =='Hyundai':
-    selectt1 = str1.selectbox('Model Name', ['All New Santro', 'Creta', 'Grand i10','i20'],key=1)
+    selectt1 = str1.selectbox('Model Name', ['All New Santro', 'Creta', 'Grand i10','i20'],key=8)
 if selectt == 'Ford':
-    selectt1 = str1.selectbox('Model Name', ['Ecosports', 'Figo',],key=1)
+    selectt1 = str1.selectbox('Model Name', ['Ecosports', 'Figo',],key=12)
 if selectt == 'Honda':
-    selectt1 = str1.selectbox('Model Name', ['Amaze', 'City(2014)', 'WR-V'],key=1)
+    selectt1 = str1.selectbox('Model Name', ['Amaze', 'City(2014)', 'WR-V'],key=16)
 if selectt == 'KIA':
-    selectt1 = str1.selectbox('Model Name', ['Carnival'],key=1)
+    selectt1 = str1.selectbox('Model Name', ['Carnival'],key=20)
 
-selectt2 = str1.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'],key=1)
-selectt4 = str1.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'],key=1)
+selectt2 = str1.selectbox('City', ['Mumbai','Delhi','Srinagar','Shimla','Vishakhapattnam'],key=11)
+selectt4 = str1.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'],key=13)
 selectt3 = str1.slider("Age of the vehicle.", 0, 200)
 
 str1.write("CAR 2")
-selecttt = str1.selectbox('Company', ['Hyundai', 'Ford', 'Honda', 'KIA'],key=5)
+selecttt = str1.selectbox('Company', ['Hyundai', 'Ford', 'Honda', 'KIA'],key=15)
 if selecttt == 'Hyundai':
-    selecttt1 = str1.selectbox('Model Name.', ['All New Santro', 'Creta', 'Grand i10', 'i20'],key=2)
+    selecttt1 = str1.selectbox('Model Name.', ['All New Santro', 'Creta', 'Grand i10', 'i20'],key=20)
 if selectt == 'Ford':
-    selecttt1 = str1.selectbox('Model Name.', ['Ecosports', 'Figo', ],key=2)
+    selecttt1 = str1.selectbox('Model Name.', ['Ecosports', 'Figo', ],key=21)
 if selecttt == 'Honda':
-    selecttt1 = str1.selectbox('Model Name.', ['Amaze', 'City(2014)', 'WR-V'],key=2)
+    selecttt1 = str1.selectbox('Model Name.', ['Amaze', 'City(2014)', 'WR-V'],key=22)
 if selectt == 'KIA':
-    selecttt1 = str1.selectbox('Model Name.', ['Carnival'],key=2)
+    selecttt1 = str1.selectbox('Model Name.', ['Carnival'],key=23)
 
-selecttt2 = str1.selectbox('City', ['Mumbai', 'Delhi', 'Srinagar', 'Shimla', 'Vishakhapattnam'],key=7)
-selecttt4 = str1.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'],key=8)
+selecttt2 = str1.selectbox('City', ['Mumbai', 'Delhi', 'Srinagar', 'Shimla', 'Vishakhapattnam'],key=17)
+selecttt4 = str1.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'],key=18)
 selecttt3 = str1.slider("Age of the vehicle", 0, 200)
 
 if str1.button('Compare'):
