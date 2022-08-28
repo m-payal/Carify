dee14cd61f9e70fe3f2a0cf839676e72c460857c
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2022-08-28 10:34:17 +0530

    Update vehicle_maintenence.py

--- vehicle_maintenence.py
+++ vehicle_maintenence.py
@@ -199,7 +199,7 @@ def compare():
     if selectt == 'KIA':
         selecttt1 = str1.selectbox('Model Name.', ['Carnival'])
 
-    sel2 = str1.selectbox('City', ['Mumbai', 'Delhi', 'Srinagar', 'Shimla', 'Vishakhapattnam'],key='0x61')
+    sel2 = str1.selectbox('City', ['Mumbai', 'Delhi', 'Srinagar', 'Shimla', 'Vishakhapattnam'],key=17)
     sel4 = str1.selectbox('Fuel', ['Petrol','1.1 Petrol','1.2L Petrol','1.5L Petrol','Diesel','1.4L Diesel','1.5L Diesel','2.2L Diesel'])
     sel3 = str1.slider("Age of the vehicle", 0, 200)
 
@@ -246,9 +246,6 @@ def about():
     st.write("In Today’s world with so many cars, models in the market, it is hard to find out which car has a high maintenance cost/index that is authentic source.While buying a car we spend a lot of time different features and also maintenance. But there's no platform which tells us the maintenance cost that we'll have to pay after buying the car. A system that shows the health and Maintenance Index of various components of car models or car parts based on multiple factors is needed. We have created this platform for all these problems")
     st.write("In our system we show the health and maintenance Index of various components of car models or car parts based on multiple factors.Also the probablity of replacement of a particular car component after the car is bought. We can also compare two different car models based on maintenence.This will help new buyers to understand the maintenance costs of a certain model and probability of which car part requires more often servicing /change, OEM’s to understand which part is requiring frequent change so they can improve that component and needs to be recalled and made better in the new models ")
     st.subheader('"Know your car\'s life before you buy it!"')
-    st.text("\n")
-    st.text("\n")
-    st.text("\n")
     st.text("\n")
     st.text("\n")
     st.text(" \n BY TEAM \n Shreya Pawaskar \n Akshata Jedhe \n Juee Ashtaputre \n Payal Mehta \n Riya Kulkarni")
